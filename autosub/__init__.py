62e64af2d88642102c0b05249f4d9fae0245a1af
ww <qingfenghuohu@126.com>
2021-02-19 23:52:05 +0800

    切换翻译源

--- autosub/__init__.py
+++ autosub/__init__.py
@@ -91,26 +91,30 @@ class SpeechRecognizer(object): # pylint: disable=too-few-public-methods
         self.retries = retries
 
     def __call__(self, data):
+        gTrans = GoogleTrans()
+
         try:
             for _ in range(self.retries):
-                url = GOOGLE_SPEECH_API_URL.format(lang=self.language, key=self.api_key)
-                headers = {"Content-Type": "audio/x-flac; rate=%d" % self.rate}
-
-                try:
-                    resp = requests.post(url, data=data, headers=headers)
-                except requests.exceptions.ConnectionError:
-                    continue
-
-                for line in resp.content.decode('utf-8').split("\n"):
-                    try:
-                        line = json.loads(line)
-                        line = line['result'][0]['alternative'][0]['transcript']
-                        return line[:1].upper() + line[1:]
-                    except IndexError:
-                        # no result
-                        continue
-                    except JSONDecodeError:
-                        continue
+               line = gTrans.query(data)
+               return line
+                # url = GOOGLE_SPEECH_API_URL.format(lang=self.language, key=self.api_key)
+                # headers = {"Content-Type": "audio/x-flac; rate=%d" % self.rate}
+                #
+                # try:
+                #     resp = requests.post(url, data=data, headers=headers)
+                # except requests.exceptions.ConnectionError:
+                #     continue
+                #
+                # for line in resp.content.decode('utf-8').split("\n"):
+                #     try:
+                #         line = json.loads(line)
+                #         line = line['result'][0]['alternative'][0]['transcript']
+                #         return line[:1].upper() + line[1:]
+                #     except IndexError:
+                #         # no result
+                #         continue
+                #     except JSONDecodeError:
+                #         continue
 
         except KeyboardInterrupt:
             return None
@@ -132,18 +136,20 @@ class Translator(object): # pylint: disable=too-few-public-methods
         try:
             if not sentence:
                 return None
-
-            result = self.service.translations().list( # pylint: disable=no-member
-                source=self.src,
-                target=self.dst,
-                q=[sentence]
-            ).execute()
-
-            if 'translations' in result and result['translations'] and \
-                'translatedText' in result['translations'][0]:
-                return result['translations'][0]['translatedText']
-
-            return None
+            gTrans = GoogleTrans()
+            result = gTrans.query(sentence)
+            return result
+            # result = self.service.translations().list( # pylint: disable=no-member
+            #     source=self.src,
+            #     target=self.dst,
+            #     q=[sentence]
+            # ).execute()
+            #
+            # if 'translations' in result and result['translations'] and \
+            #     'translatedText' in result['translations'][0]:
+            #     return result['translations'][0]['translatedText']
+            #
+            # return None
 
         except KeyboardInterrupt:
             return None
@@ -409,6 +415,73 @@ def main():
 
     return 0
 
+
+class Py4Js:
+    def __init__(self):
+        self.ctx = execjs.compile("""
+        function TL(a) {
+        var k = "";
+        var b = 406644;
+        var b1 = 3293161072;
+
+        var jd = ".";
+        var $b = "+-a^+6";
+        var Zb = "+-3^+b+-f";
+
+        for (var e = [], f = 0, g = 0; g < a.length; g++) {
+            var m = a.charCodeAt(g);
+            128 > m ? e[f++] = m : (2048 > m ? e[f++] = m >> 6 | 192 : (55296 == (m & 64512) && g + 1 < a.length && 56320 == (a.charCodeAt(g + 1) & 64512) ? (m = 65536 + ((m & 1023) << 10) + (a.charCodeAt(++g) & 1023),
+            e[f++] = m >> 18 | 240,
+            e[f++] = m >> 12 & 63 | 128) : e[f++] = m >> 12 | 224,
+            e[f++] = m >> 6 & 63 | 128),
+            e[f++] = m & 63 | 128)
+        }
+        a = b;
+        for (f = 0; f < e.length; f++) a += e[f],
+        a = RL(a, $b);
+        a = RL(a, Zb);
+        a ^= b1 || 0;
+        0 > a && (a = (a & 2147483647) + 2147483648);
+        a %= 1E6;
+        return a.toString() + jd + (a ^ b)
+    };
+
+    function RL(a, b) {
+        var t = "a";
+        var Yb = "+";
+        for (var c = 0; c < b.length - 2; c += 3) {
+            var d = b.charAt(c + 2),
+            d = d >= t ? d.charCodeAt(0) - 87 : Number(d),
+            d = b.charAt(c + 1) == Yb ? a >>> d: a << d;
+            a = b.charAt(c) == Yb ? a + d & 4294967295 : a ^ d
+        }
+        return a
+    }
+    """)
+
+    def getTk(self, text):
+        return self.ctx.call("TL", text)
+
+
+class GoogleTrans:
+    def translate(self, tk, content):
+        if len(content) > 4891:
+            print("翻译的长度超过限制！！！")
+            return
+
+        param = {'tk': tk, 'q': content}
+
+        result = requests.get(
+            """http://translate.google.cn/translate_a/single?client=t&tl=zh-CN&hl=zh-CN&dt=at&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&ie=UTF-8&oe=UTF-8&clearbtn=1&otf=1&pc=1&srcrom=0&ssel=0&tsel=0&kc=2&sl=auto""",
+            params=param)
+        res = result.json()
+        # 返回的结果为Json，解析为一个嵌套列表
+        return res[0][0][0]
+
+    def query(self, content):
+        js = Py4Js()
+        tk = js.getTk(content)
+        return self.translate(tk, content)
 
 if __name__ == '__main__':
     sys.exit(main())
